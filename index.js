473d50fa21e220b8b0f13302e2b005c363c333c9
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-03-19 20:31:08 -0700

    Add autocomplete and nearby bank search

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Global variables for Google Maps
 let map;
-
+let infowindow;
+let places;
 
 //Initiate Google Maps default map location
 function initMap() {
@@ -9,3 +10,35 @@ function initMap() {
     center: defaultPosition,
     zoom: 15
   });
+  //const userInput = $('#js-current-location');
+const autocomplete = new google.maps.places.Autocomplete(document.getElementById('js-current-location'));
+const places = new google.maps.places.PlacesService(map)
+autocomplete.addListener('place_changed', onPlaceChanged);
+
+function onPlaceChanged() {
+  let place = autocomplete.getPlace();
+  if (place.geometry) {
+    map.panTo(place.geometry.location);
+    map.setZoom(15);
+    }
+  else if (!place.geometry) {
+    window.alert("No results available for '" + place.name + "'");
+    return;
+  }
+    infowindow = new google.maps.InfoWindow();
+    places.nearbySearch({
+          location: place.geometry.location,
+          bounds: map.getBounds(),
+          type: ['bank']
+        }, callback);
+  }
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
