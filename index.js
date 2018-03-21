ff886574460b6ae6c8855232601ddb49444d4c03
Ryan Okamuro <ryaniscool003@yahoo.com>
2018-03-20 21:37:11 -0700

    Add marker timeout & currency pairs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Get JSON & using Ajax
-function getDataFromApi(currencyForex) {
+function getDataFromApi() {
   const settings = {
     'async': true,
     'crossDomain': true,
@@ -9,31 +9,42 @@ function getDataFromApi(currencyForex) {
       'Authorization': 'Bearer 5qCpoR1yN5ePigTWl2G1kG5T5tX8fAuV',
       'Cache-Control': 'no-cache',
     },
-    'success': function(data){ convertCurrency(data) }
+    'success': function(data){convertCurrency(data)}
   }
   $.ajax(settings);
 }
 
-//function to create convert currency
+//List of currencies application supports
+const currencySymbols =  
+['AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 
+'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 
+'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLP', 'CNY', 'COP', 'CRC', 'CUP', 'CVE', 
+'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 
+'GBP', 'GEL', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 
+'HTG', 'HUF', 'IDR', 'ILS', 'INR', 'IQD', 'IRR', 'ISK', 'JMD', 'JOD', 'JPY', 
+'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 
+'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 
+'MRO', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 
+'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 
+'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 
+'SLL', 'SOS', 'SRD', 'STD', 'SVC', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 
+'TOP', 'TRY', 'TTD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VEF', 
+'VND', 'VUV', 'WST', 'XAF', 'BTC', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 
+'ZMW', 'ZWD'];
+
+//Function to create convert currency
 function convertCurrency(result) {
   let converted = `
   <form role='form' class='exchangeConversion'>
   <fieldset name='convertCurrency'>
     <legend>Currency Exchange</legend> 
     <label for='js-homeland-currency' class='home_currency'></label>
-    <table class='table'>
-      <thead>
-        <tr><th class='home_money'></th>USD<th class='travel_money'>CNY</th></tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td id='homeland_money'></td>$1.00<td id='travelingCountry_money'>${result.value}</td>
-        </tr>
-      </tbody>
-    </table>
-    <section role="region" class="oneHomelandExchange"></section>
+    <input placeholder='0.00' type='number' name='js-homeland-currency' id='js-homeland-currency' />
   </fieldset>
-  </form>
+  </form> 
+  <section role='region' class='foreignExchangeTotal'>
+  <p class='afterExchange'>${result.value}</p>
+  </section> 
   `;
   $('#travel-currency').html(converted);
 }
@@ -45,8 +56,8 @@ function activateExchangeWindow() {
     getDataFromApi();
     $('.travelex').hide();
 //return value of current currency
-    //const userInput = $(this).find('#js-home-currency')
-    //$('.home_currency').text(`${userInput.val()} Amount`);
+    const userInput = $(this).find('#js-home-currency')
+    $('.home_currency').text(`${userInput.val()}`);
     //$('.home_money').text(`${userInput.val()}`)
     //const userInput = $(this).find('#js-current-country')
     //$('.travel_money').text(`${userInput.val()}`)
@@ -113,8 +124,13 @@ function createMarker(place) {
   placesList.appendChild(li);
   li.onclick= function() {
     google.maps.event.trigger(marker, 'click');
-    //HOW TO MAKE ANIMATION STOP BOUNCING
-    //marker.setAnimation(google.maps.Animation.BOUNCE);
+//Make markers bounce when clicking on right-hand panel 
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function ()
+        {
+            marker.setAnimation(null);
+            $(marker).dequeue();
+        }, 1400);
   };
 
   google.maps.event.addListener(marker, 'click', function() {
